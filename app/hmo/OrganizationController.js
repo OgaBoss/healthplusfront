6eac4cc70d45a089d031cd5ef7db1095f53061d0
damilola adebayo <OluwadamilolaAdebayo@damilolas-MacBook-Pro.local>
2016-09-15 20:15:16 +0100

    Fixing UI

--- app/hmo/OrganizationController.js
+++ app/hmo/OrganizationController.js
@@ -1,19 +1,9 @@
 /**
  * Created by OluwadamilolaAdebayo on 9/6/16.
  */
-app.controller('OrganizationController', function($scope, healthNotify){
+app.controller('OrganizationController', function($scope, healthNotify, $timeout){
 
     var vm = this;
-
-    $scope.hoverIn = function(e){
-        var elm_class = $(e.target).data('id');
-        console.log(elm_class);
-        angular.element(document.querySelector(".plan2")).addClass("featured");
-    }
-
-    $scope.hoverOut = function(e){
-        angular.element(document.querySelector(".plan2")).removeClass("featured");
-    }
 
     $scope.update = function(){
         healthNotify.set('Data Updated successfully', 'success');
@@ -61,4 +51,47 @@ app.controller('OrganizationController', function($scope, healthNotify){
             vm.inActiveEnrollees = true
         }
     }
+
+    $timeout(function() {
+        $scope.renderChartOrg= true;
+        console.log('Rendering chart')
+    }, 1000);
+
+    $scope.labels = ['Hospital', 'Pharmacy'];
+    $scope.data = [200, 150];
+
+    $scope.colors = ['#F7464A', '#46BFBD'];
+    // Chart.js Options - complete list at http://www.chartjs.org/docs/
+    $scope.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltipFontSize: 11,
+        tooltipFontFamily: "'Helvetica', 'Arial', sans-serif",
+        tooltipCornerRadius: 0,
+        tooltipCaretSize: 2,
+        segmentShowStroke: true,
+        segmentStrokeColor: '#fff',
+        segmentStrokeWidth: 2,
+        percentageInnerCutout: 50,
+        animationSteps: 100,
+        animationEasing: 'easeOutBounce',
+        animateRotate: true,
+        animateScale: false
+    };
+
+    $scope.dates = {
+        startDate: moment('2013-09-20'),
+        endDate: moment('2013-09-25')
+    };
+    $scope.dates2 = {
+        startDate: moment().subtract(1, 'day').format('MM/DD/YYYY'),
+        endDate: moment().subtract(1, 'day').format('MM/DD/YYYY')
+    };
+    $scope.ranges = {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 days': [moment().subtract(7, 'days'), moment()],
+        'Last 30 days': [moment().subtract(30, 'days'), moment()],
+        'This month': [moment().startOf('month'), moment().endOf('month')]
+    };
 })
